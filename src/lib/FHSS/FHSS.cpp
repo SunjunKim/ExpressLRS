0b1b058983b278cc5a1f863d5be2e78e091adb7c
Sunjun Kim <SunjunKim@users.noreply.github.com>
2025-07-07 16:01:46 +0900

    further chagne 1280 freq domains

--- src/lib/FHSS/FHSS.cpp
+++ src/lib/FHSS/FHSS.cpp
@@ -38,7 +38,7 @@ const fhss_config_t domains[] = {
     #elif defined(Regulatory_Domain_ISM_2400)
         "KC2G4",
     #endif
-    FREQ_HZ_TO_REG_VAL(2420400000), FREQ_HZ_TO_REG_VAL(2479400000), 80, 2440000000}
+    FREQ_HZ_TO_REG_VAL(2420400000), FREQ_HZ_TO_REG_VAL(2479400000), 60, 2450000000}
 };
 #endif
 
