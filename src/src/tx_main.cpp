c426835eea262f56cec0d75f4f793f8f41201c5a
Sunjun Kim <SunjunKim@users.noreply.github.com>
2026-03-16 21:52:46 +0900

    for rx mode test

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -1504,12 +1504,14 @@ void setup()
   Radio.End();
 
   Radio.TXdoneCallback = [](){};
-  Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
+  
+  /* Block for CW - uncomment it for the CW output, leave the comment for RX test mode */
+  // Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
 
-  POWERMGNT::init();
-  POWERMGNT::setPower(POWERMGNT::getMinPower());
+  // POWERMGNT::init();
+  // POWERMGNT::setPower(POWERMGNT::getMinPower());
 
-  Radio.startCWTest(2440000000, SX12XX_Radio_1);
+  // Radio.startCWTest(2440000000, SX12XX_Radio_1);
 }
 
 void loop()
